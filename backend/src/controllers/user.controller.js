0b7e7eb413720fd45669e43d12639d594ee577cf
rahul-adepu <rahuladepu002@gmail.com>
2025-05-21 18:09:42 +0530

    Done with password encryption

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,20 +1,38 @@
+const bcrypt = require("bcrypt");
 const { User } = require("../entities/User");
-
 const AppDataSource = require("../config/db");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const userRepository = AppDataSource.getRepository("User");
 
 const registerUser = async (req, res) => {
   try {
-    const { username, role, password } = req.body;
+    const { username, password, role } = req.body;
 
-    const newUser = userRepository.create({ username, password, role });
+    const existingUser = await userRepository.findOneBy({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already registered" });
+    }
+
+    const hashedPassword = await bcrypt.hash(
+      password,
+      parseInt(process.env.SALT_ROUNDS)
+    );
+
+    const newUser = userRepository.create({
+      username,
+      password: hashedPassword,
+      role,
+    });
+
     await userRepository.save(newUser);
 
     return res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    console.log("Error while registering user", error);
-    return res.status(500).json({ error: error.message });
+    console.error("Registration error:", error);
+    return res.status(500).json({ message: "Something went wrong" });
   }
 };
 
