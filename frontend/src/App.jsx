6c9184db5c77430e7bef47caa5cbd3227acddb86
rahul-adepu <rahuladepu002@gmail.com>
2025-05-22 22:54:01 +0530

    Update routing: Signup page displayed on / path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import CreateSoftware from "./pages/CreateSoftware";
 import PendingRequests from "./pages/PendingRequests";
 import RequestAccess from "./pages/RequestAccess";
@@ -12,6 +12,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate to="/signup" />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/create-software" element={<CreateSoftware />} />
